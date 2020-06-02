9879775f1f9463a715424e5ff8b7df7af637382a
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-02 18:54:00 +0700

    modification of NhonLV

--- Resi/src/network/layers/DataLinkLayer.java
+++ Resi/src/network/layers/DataLinkLayer.java
@@ -1,10 +1,14 @@
 package network.layers;
 
-import network.Packet;
+import network.elements.Packet;
 
 public class DataLinkLayer {
+	public Packet packet;
+	// cap nhat thong tin packet
+	public DataLinkLayer(Packet p){
+		this.packet = p;
+	}
 	public void update(Packet p)
 	{
-		
 	}
 }
