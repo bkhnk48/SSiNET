87e15f328d709e0823c81e93c4e3a8191da125c8
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-02 18:54:09 +0700

    modification of NhonLV

--- Resi/src/network/layers/NetworkLayer.java
+++ Resi/src/network/layers/NetworkLayer.java
@@ -1,9 +1,64 @@
 package network.layers;
 
-import network.Packet;
+import config.Constant;
+import custom.fattree.FatTreeRoutingAlgorithm;
+import events.EMovingInSwitchEvent;
+import infrastructure.entity.Node;
+import infrastructure.event.Event;
+import network.elements.EntranceBuffer;
+import network.elements.ExitBuffer;
+import network.elements.Packet;
+import network.entities.Switch;
 
 public class NetworkLayer {
-	public void controlFlow() {}
-	
-	public void route(Packet p) {}
+	protected FatTreeRoutingAlgorithm fatTreeRoutingAlgorithm;
+
+	public NetworkLayer(FatTreeRoutingAlgorithm fatTreeRoutingAlgorithm) {
+		this.fatTreeRoutingAlgorithm = fatTreeRoutingAlgorithm;
+	}
+
+	public void controlFlow(ExitBuffer exitBuffer) {
+		if (!(exitBuffer.isRequestListEmpty())) {
+			int selectedId = Integer.MAX_VALUE;
+			EntranceBuffer selectedENB  = null;
+			Packet p;
+			// lay ra cac enb tu request list cua exb hien tai
+			for (EntranceBuffer enb : exitBuffer.getRequestList()) {
+				p = enb.getPeekPacket();
+				// chon ra Inport co Packet co id nho nhat
+				if (p != null && !(enb.hasEventOfPacket(p))) {
+					if (p.getId() < selectedId) {
+						selectedId = p.getId();
+						selectedENB = enb;
+					}
+				}
+			}
+			if(selectedENB != null) {
+				long time = selectedENB.physicalLayer.simulator.time();
+				Event event = new EMovingInSwitchEvent(time, time + Constant.SWITCH_CYCLE,
+						selectedENB, selectedENB.getPeekPacket());
+				selectedENB.insertEvents(event); //chen them su kien moi vao
+			}
+		}
+	}
+
+	public void route(EntranceBuffer entranceBuffer) {
+		if (entranceBuffer.getNextNodeId() == -1) {
+			Packet packet = entranceBuffer.getPeekPacket();
+			if((packet ==null)){
+				System.out.println("ERROR: 2");
+			}
+			int nextNodeID = fatTreeRoutingAlgorithm.next(packet.getSource(), entranceBuffer.getNode().getId(), packet.getDestination());
+
+			entranceBuffer.setNextNode(nextNodeID);
+
+			ExitBuffer exitBuffer = entranceBuffer.physicalLayer.exitBuffers.get(nextNodeID);
+			exitBuffer.addToRequestList(entranceBuffer);
+			controlFlow(exitBuffer);
+		} else {
+			ExitBuffer exitBuffer = entranceBuffer.physicalLayer.exitBuffers
+					.get(entranceBuffer.getNextNodeId());
+			controlFlow(exitBuffer);
+		}
+	}
 }
