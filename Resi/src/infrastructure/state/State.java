747917de4aa26286f6612c0d82bb5c3b7642de62
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-02 19:22:59 +0700

    State might not need any subclass

--- Resi/src/infrastructure/state/State.java
+++ Resi/src/infrastructure/state/State.java
@@ -3,7 +3,7 @@ package infrastructure.state;
 import infrastructure.element.Element;
 import infrastructure.event.Event;
 
-public abstract class State {
+public class State {
 	//public Event ancestorEvent;
 	public Element element;
 	public void act() {}
