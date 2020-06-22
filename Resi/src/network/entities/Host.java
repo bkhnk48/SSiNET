1c86e2b37077014c46df7dd66d5343c1d2243e35
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-22 16:11:48 +0700

    all host could generate packets instead of only source node

--- Resi/src/network/entities/Host.java
+++ Resi/src/network/entities/Host.java
@@ -31,4 +31,16 @@ public class Host extends Node {
    {
 	   
    }
+   
+   public void generatePacket(int destination)
+   {
+       if(this.physicalLayer.sourceQueue == null)
+           this.physicalLayer.sourceQueue = new SourceQueue(this.id, destination);
+       else
+           this.physicalLayer.sourceQueue.setDestinationID(destination);
+
+       long time = (long)this.physicalLayer.simulator.time();
+       Event ev = new AGenerationEvent(this.physicalLayer.simulator, time, time, this.physicalLayer.sourceQueue);
+       this.physicalLayer.sourceQueue.insertEvents(ev);
+   }
 }
