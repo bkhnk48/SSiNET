d8f914747515d304adb7ab25737dbcb63b439c81
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-22 15:23:46 +0700

    prepare for pair hosts off

--- Resi/src/network/Topology.java
+++ Resi/src/network/Topology.java
@@ -48,63 +48,8 @@ public class Topology {
         //Endof ThanhNT 14/10 add new statements to init property
 
         NetworkLayer networkLayer = new NetworkLayer(routingAlgorithm);
-
-        // khoi tao host va dua vao list
-        Integer[] hostIDList = graph.hosts().toArray(new Integer[0]);
-
-
-        Knuth.shuffle(hostIDList);
-
-//        hostIDList = new Integer[]{ 17,24,18,11,2,3,19,8,26,0,27,1,10,16,9,25 };
-
-        sourceNodes.addAll(Arrays.asList(hostIDList).subList(0, hostIDList.length / 2));
-        //sourceNodes.add(0);
         
-        for (int sourceNodeID : sourceNodes) {
-            SourceNode sourceNode = new SourceNode(sourceNodeID);
-            sourceNode.physicalLayer = new PhysicalLayer(sourceNode);
-            sourceNode.networkLayer = networkLayer;
-            hosts.add(sourceNode);
-            hostById.put(sourceNodeID, sourceNode);
-
-            //ThanhNT 14/10 add new statements to add new ID of HOST
-            cordOfNodes.put(sourceNodeID, "");
-            //Endof ThanhNT 14/10 add new statements to add new ID of HOST
-
-        }
-
-        destinationNodes.addAll(Arrays.asList(hostIDList).subList(hostIDList.length / 2, hostIDList.length));
-        //destinationNodes.add(1);
-        
-        for (int destinationNodeID : destinationNodes) {
-            DestinationNode destinationNode = new DestinationNode(destinationNodeID);
-            hosts.add(destinationNode);
-            hostById.put(destinationNodeID, destinationNode);
-
-            //ThanhNT 14/10 add new statements to add new ID of HOST
-            cordOfNodes.put(destinationNodeID, "");
-            //Endof ThanhNT 14/10 add new statements to add new ID of HOST
-
-            destinationNode.physicalLayer = new PhysicalLayer(destinationNode);
-            destinationNode.networkLayer = networkLayer;
-        }
-
-        // khoi tao switch va them vao list
-        for (int sid : graph.switches()) {
-            Switch sw = new Switch(sid);
-            switches.add(sw);
-            switchById.put(sid, sw);
-
-            //ThanhNT 14/10 add new statements to add new ID of switch
-            cordOfNodes.put(sid, "");
-            //Endof ThanhNT 14/10 add new statements to add new ID of switch
-
-            sw.physicalLayer = new PhysicalLayer(sw, graph.getK());
-            sw.networkLayer = networkLayer;
-        }
-
-
-        // link from switch to switch
+     // link from switch to switch
         Coordination C = new Coordination(graph);
         for (Switch sw : switches) {
             for (int nextNodeID : graph.adj(sw.getId())) {
@@ -148,6 +93,65 @@ public class Topology {
                 }
             }
         }
+
+        // khoi tao host va dua vao list
+        Integer[] hostIDList = graph.hosts().toArray(new Integer[0]);
+
+
+        //Knuth.shuffle(hostIDList);
+
+//        hostIDList = new Integer[]{ 17,24,18,11,2,3,19,8,26,0,27,1,10,16,9,25 };
+
+        sourceNodes.addAll(Arrays.asList(hostIDList)//.subList(0, hostIDList.length / 2)
+        											);
+        //sourceNodes.add(0);
+        
+        for (int sourceNodeID : sourceNodes) {
+            SourceNode sourceNode = new SourceNode(sourceNodeID);
+            sourceNode.physicalLayer = new PhysicalLayer(sourceNode);
+            sourceNode.networkLayer = networkLayer;
+            hosts.add(sourceNode);
+            hostById.put(sourceNodeID, sourceNode);
+
+            //ThanhNT 14/10 add new statements to add new ID of HOST
+            cordOfNodes.put(sourceNodeID, "");
+            //Endof ThanhNT 14/10 add new statements to add new ID of HOST
+
+        }
+
+        destinationNodes.addAll(Arrays.asList(hostIDList)//.subList(hostIDList.length / 2, hostIDList.length)
+        						);
+        //destinationNodes.add(1);
+        
+        for (int destinationNodeID : destinationNodes) {
+            DestinationNode destinationNode = new DestinationNode(destinationNodeID);
+            hosts.add(destinationNode);
+            hostById.put(destinationNodeID, destinationNode);
+
+            //ThanhNT 14/10 add new statements to add new ID of HOST
+            cordOfNodes.put(destinationNodeID, "");
+            //Endof ThanhNT 14/10 add new statements to add new ID of HOST
+
+            destinationNode.physicalLayer = new PhysicalLayer(destinationNode);
+            destinationNode.networkLayer = networkLayer;
+        }
+
+        // khoi tao switch va them vao list
+        for (int sid : graph.switches()) {
+            Switch sw = new Switch(sid);
+            switches.add(sw);
+            switchById.put(sid, sw);
+
+            //ThanhNT 14/10 add new statements to add new ID of switch
+            cordOfNodes.put(sid, "");
+            //Endof ThanhNT 14/10 add new statements to add new ID of switch
+
+            sw.physicalLayer = new PhysicalLayer(sw, graph.getK());
+            sw.networkLayer = networkLayer;
+        }
+
+
+        
 
         // todo them phan add link tu switch den host kieu nhu o tren
         // link from switch to host
