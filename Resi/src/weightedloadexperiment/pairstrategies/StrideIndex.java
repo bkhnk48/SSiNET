7bb96aa8929c9106f0652e1fc32a3063dbfc5885
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-22 14:42:44 +0700

    new class for pairing off stride index

---
+++ Resi/src/weightedloadexperiment/pairstrategies/StrideIndex.java
@@ -0,0 +1,31 @@
+package weightedloadexperiment.pairstrategies;
+
+import java.util.List;
+
+public class StrideIndex extends PairGenerator {
+    private int stride;
+    private int modulo ;
+    public StrideIndex(Integer[] allHosts, int stride)
+    {
+        super(allHosts);
+        this.stride = stride;
+        modulo = allHosts.length;
+    }
+
+    public void pairHosts()
+    {
+        List<Integer> sources = getSources();
+        List<Integer> destinations = getDestinations();
+
+        Integer[] hosts = getAllHosts();
+        for(int i = 0; i < hosts.length; i++)
+        {
+            int x = hosts[i];
+            sources.add(x);
+            destinations.add(hosts[(i + stride) % modulo]);
+        }
+
+        setSources(sources);
+        setDestinations(destinations);
+    }
+}
