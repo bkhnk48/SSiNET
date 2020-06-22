b67de9201c4cd4a7a48d6e10d920c2f21292547e
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-22 18:08:30 +0700

    new code bc combination between SourceNode and DestinationNode

--- Resi/src/network/entities/Host.java
+++ Resi/src/network/entities/Host.java
@@ -1,8 +1,10 @@
 package network.entities;
 
 import infrastructure.event.Event;
+import config.Constant;
 import events.AGenerationEvent;
 import infrastructure.entity.Node;
+import network.elements.Packet;
 import network.elements.SourceQueue;
 import network.layers.PhysicalLayer;
 
@@ -15,7 +17,7 @@ import network.layers.PhysicalLayer;
 
 public class Host extends Node {
 
-
+	public TypeOfHost type;
 
 	public Host(int id) {
 	     super(id);
@@ -27,10 +29,7 @@ public class Host extends Node {
 
     }
    
-   public void receive()
-   {
-	   
-   }
+   
    
    public void generatePacket(int destination)
    {
@@ -42,5 +41,52 @@ public class Host extends Node {
        long time = (long)this.physicalLayer.simulator.time();
        Event ev = new AGenerationEvent(this.physicalLayer.simulator, time, time, this.physicalLayer.sourceQueue);
        this.physicalLayer.sourceQueue.insertEvents(ev);
+   }
+   
+   protected int receivedPacketInNode = 0;
+   protected double lastRx = 0; // thoi gian goi tin cuoi cung den host
+   protected double firstTx = -1; //la thoi gian goi tin dau tien den host
+
+   
+
+   public int getReceivedPacketInNode() {
+       return receivedPacketInNode;
+   }
+
+   public double getLastRx() {
+       return lastRx;
+   }
+
+   public double getFirstTx() {
+       return firstTx;
+   }
+
+   public void receivePacket(Packet packet){
+       double currentTime = this.physicalLayer.simulator.getTime();
+       this.physicalLayer.simulator.numReceived++;
+       if(this.receivedPacketInNode == 0) {
+//           this.firstTx = packet.getStartTime();
+           this.firstTx = currentTime;
+           //System.out.println("Thoi gian goi tin dau tien den voi host " + self.id + " la: " + this.firstTx);
+       }
+       this.receivedPacketInNode ++;
+       this.lastRx = currentTime;
+       this.physicalLayer.simulator.receivedPacketPerUnit[(int)(currentTime / Constant.EXPERIMENT_INTERVAL + 1)]++;
+       packet.setEndTime(currentTime);
+
+       this.physicalLayer.simulator.totalPacketTime += packet.timeTravel();
+       this.physicalLayer.simulator.totalHop += packet.nHop; //todo xem nHop la gi
+
+   }
+   
+   @Override
+   public boolean isDestinationNode()
+   {
+	   return (this.type == TypeOfHost.Destionation || this.type == TypeOfHost.Mix);
+   }
+   
+   public boolean isSourceNode()
+   {
+	   return (this.type == TypeOfHost.Source || this.type == TypeOfHost.Mix);
    }
 }
