937e0562b8e94d75198331821fcf639b2c5e1f2f
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-06-26 16:47:25 +0700

    adjust for oversubscription

--- Resi/src/config/Constant.java
+++ Resi/src/config/Constant.java
@@ -8,8 +8,8 @@ public class Constant {
     // Switch delay ~ 100ns
     public static final int SWITCH_DELAY = 100;
     // Link bandwidth, set default to 1Gps
-    public static final long LINK_BANDWIDTH = (long) 1e9;
-
+    public static final long LINK_BANDWIDTH = //(long) 1e9;//1Gb
+    											96*1000*1000;
     // Default length of link ~ 5m
     public static final double DEFAULT_LINK_LENGTH = 5;
     public static final double HOST_TO_SWITCH_LENGTH = 0.1;
@@ -21,8 +21,8 @@ public class Constant {
     public static final int HOST_DELAY = 100000;//ns => 10^5 * 10^(-9) = 10^(-4)(s)
 
     // Packet size ~ 1Mb
-    public static final int PACKET_SIZE = (int) 1e5; // 100Kb
-
+    public static final int PACKET_SIZE = //(int) 1e5; // 100Kb
+    										9600; //9.6Kb
     // Maximum time system
     public static final int MAX_TIME = (int) 1e9;
 
