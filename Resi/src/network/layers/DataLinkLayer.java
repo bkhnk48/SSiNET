47df91953a8e021110bdf9796da2b7d356f3488b
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-08-17 20:02:19 +0700

    this class extends Layer as of right now

--- Resi/src/network/layers/DataLinkLayer.java
+++ Resi/src/network/layers/DataLinkLayer.java
@@ -1,14 +1,23 @@
 package network.layers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javatuples.Pair;
 import network.elements.Packet;
 
-public class DataLinkLayer {
+public class DataLinkLayer extends Layer {
 	public Packet packet;
 	// cap nhat thong tin packet
 	public DataLinkLayer(Packet p){
 		this.packet = p;
 	}
+	
+	Map<Pair<Integer, Integer>, Integer> flowSizesPerDuration = new HashMap<>();
+	Map<Integer, Integer> outgoingTraffic = new HashMap();
+	
 	public void update(Packet p)
 	{
+		
 	}
 }
