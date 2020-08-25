e40bb3637087bc074a19315014bac1522a8980b0
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-08-25 16:35:43 +0700

    new method to the interface RoutingAlgorithm

--- Resi/src/routing/RoutingAlgorithm.java
+++ Resi/src/routing/RoutingAlgorithm.java
@@ -1,8 +1,12 @@
 package routing;
 
+import infrastructure.entity.Node;
+import network.elements.Packet;
 
 public interface RoutingAlgorithm {
     int next(int source, int current, int destination);
 
     RoutingPath path(int source, int destination);
+    
+    int next(Packet packet, Node node) ;
 }
