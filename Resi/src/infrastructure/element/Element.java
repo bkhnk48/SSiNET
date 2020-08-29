544248e94993c871bc91425f4651977ae35172cb
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-08-29 15:28:33 +0700

    implement new interface

--- Resi/src/infrastructure/element/Element.java
+++ Resi/src/infrastructure/element/Element.java
@@ -9,13 +9,14 @@ import infrastructure.state.*;
 import java.util.ArrayList;
 
 import events.EMovingInSwitchEvent;
+import events.IEventGenerator;
 
-public abstract class Element {
+public abstract class Element implements IEventGenerator {
 	protected int id;
 	protected State state;
 	protected long soonestEndTime = Long.MAX_VALUE; /// todo check NHONLV change from 0 to max
 	//public ArrayList<Event> allEvents;
-	public DiscreteEventSimulator sim;
+	//public DiscreteEventSimulator sim;
 
 	public Element(){
 		//allEvents = new ArrayList<Event>();
@@ -25,10 +26,7 @@ public abstract class Element {
 		this.id = id;
 	}
 
-	public void setState(State state)
-	{
-		this.state = state;
-	}
+	
 	
 	public void setType(Type type)
 	{
@@ -48,67 +46,31 @@ public abstract class Element {
 	public State getState() {
 		return state;
 	}
+	
+	public void setState(State state)
+	{
+		this.state = state;
+	}
 
 	public long getSoonestEndTime() { return soonestEndTime; }
 
-	public void getNextState() { }
+	
 
 	public boolean hasEventOfPacket(Packet packet)
 	{
+		DiscreteEventSimulator sim = DiscreteEventSimulator.getInstance();
 		if(sim == null) return false;
-		//if(sim.allEvents == null) return false;
-		//if(sim.allEvents.isEmpty()) return false;
+
 		if(packet == null) return false;
 		else
 		{ 
-			/*for(Event event : sim.allEvents)
-			{ 
-				if(event.getPacket() == packet) 
-					return true; 
-			}*/ 
+
 			return false; 
 		}
-		/*
-		 * if(allEvents == null) return false; else if(allEvents.isEmpty()) return
-		 * false; else if(packet == null) return false; else{ for(Event event :
-		 * allEvents){ if(event.getPacket() == packet) return true; } return false; }
-		 */
-	}
 
-	/**
-	 * Xay dung phuong thuc insertEvent thuc hien viec
-	 * chen mot Event co ten la ev.
-	 * @param ev
-	 */
-	public void insertEvents(Event ev)
-	{
-		sim.addEvent(ev);
-		/*long endTime = ev.getEndTime();
-		
-		if(sim == null)
-			return;
-		if(sim.allEvents == null)
-		{
-			sim.allEvents = new ArrayList<Event>();
-			sim.allEvents.add(ev);
-			sim.ongoingExecutionTimes.put(endTime, 0);
-			return;
-		}
-		if(sim.allEvents.size() == 0)
-		{
-			sim.allEvents.add(ev);
-			updateSoonestEndTime();
-			sim.ongoingExecutionTimes.put(endTime, 0);
-			return;
-		}
-		
-		sim.insertEvent(ev);
-		updateSoonestEndTime();*/
 	}
 
 	
 
-	
-	
 
 }
